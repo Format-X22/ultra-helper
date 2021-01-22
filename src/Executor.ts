23a3e58352da2402d203c835cc4b9e2f557a9c63
Oleg <oleg.pav.m@gmail.com>
2021-01-22 16:23:05 +0300

    Executor skeleton.

--- src/Executor.ts
+++ src/Executor.ts
@@ -1,3 +1,25 @@
+import { TaskConfig } from './TaskCalculator';
+
+export type Task = {
+    id: number;
+};
+
+const SQUEEZE_INDENT_PERCENT: number = 0.5;
+const MAX_ENTER_AMOUNT: number = 190_000;
+const STOP_INDENT_RATE: number = 2.5;
+
 export class Executor {
-    //
+    async startTask(config: TaskConfig): Promise<void> {
+        // TODO -
+    }
+
+    async cancelTask(id: number): Promise<string> {
+        // TODO -
+        return 'TODO';
+    }
+
+    async getStatus(): Promise<Array<Task>> {
+        // TODO -
+        return [{ id: 0 }, { id: 1 }];
+    }
 }
